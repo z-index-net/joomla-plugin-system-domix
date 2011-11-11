eb30219b9481f40c683013bb611c2817f14a559b
Branko Wilhelm <branko.wilhelm@gmail.com>
2011-11-11 15:28:42 +0000

    firebug und var_dump optional hinzugefügt datei ausgabe entfernt

--- domix.php
+++ domix.php
@@ -13,45 +13,41 @@
 defined('_JEXEC') or die();
 
 class domix{
+    
+    public static $count = 0;
+
 	private static $_css = array( 
 		'border-top:  1px solid #00f;',
 		'font-size:   11px;',
-		'font-family: verdana;',
-		'color:       #000;',
+		'font-family: Verdana;',
+		'color:       #000 !important;',
 		'background:  #ffffef;' 
 	);
 	
-	public static function _( &$data, $exit = false, $filename = false ){
+	public static function _( &$data, $count, $exit = false){
 		$type = gettype( $data );
 		$function = 'from' .ucfirst( $type );
 		$funccall = is_callable( 'self::' .$function ) ? $function : 'fromString';
 		$out = call_user_func( array( 
 			'self', $funccall 
 		), $data );
-		if ( $filename ) {
-			$path = JPATH_SITE.DS.'domix';
-			
-			if(!file_exists($path)) {
-				mkdir($path, 0777, true);
-			}
-			file_put_contents( $path.DS.$filename, $out );
-		} else {
-			echo '<pre style="'.implode(self::$_css).'">'."\n".$out."\n".'</pre>';
-		}
+
+		echo '<pre style="'.implode(self::$_css).'"><u>'.$count.':</u>'."\n".$out."\n".'</pre>';
+
 		if ( $exit ) exit();
 	}
-	
+    
 	private static function fromObject( $data ){
 		$echo = array();
 		$echo[] = 'Instanz von "'.get_class( $data ).'" ';
 		$parentclass = get_parent_class( $data );
 		$echo[] = $parentclass ? ' geerbt von "'.$parentclass.'"' : '';
-		$echo[] = "\nObjekt Variablen:\n";
-		$echo[] = print_r( $data, true );
-		$echo[] = "\nObjekt Methoden:\n";
+		$echo[] = "\n<u>Objekt Variablen</u>:\n";
+		$echo[] = htmlspecialchars(print_r( $data, true ));
+		$echo[] = "\n<u>Objekt Methoden</u>:\n";
 		$echo[] = '- '.print_r( implode( "\n- ", get_class_methods( get_class( $data ) ) ), true );
 		if($parentclass) {
-			$echo[] = "\nPublic Methoden von " .$parentclass .":\n";
+			$echo[] = "\n<u>Public Methoden von " .$parentclass ."</u>:\n";
 			$echo[] = '- '.print_r( implode( "\n- ", get_class_methods( $parentclass ) ), true );
 		}
 		return implode( $echo );
@@ -74,7 +70,7 @@ class domix{
 	}
 	
 	private static function fromString( $data ){
-		return ( string ) $data;
+		return htmlspecialchars( ( string ) $data);
 	}
 	
 	private static function fromArray( $data ){
@@ -84,22 +80,45 @@ class domix{
 	private static function fromResource( $data ){
 		return self::fromString( $data );
 	}
+    
+    public function counter() {
+        return ++self::$count;
+    }
 }
 
 /**
- * Speichert Variablen in eine txt-Datei
+ * Gibt Daten mit domix aus
  *
- * @param mixed $val
- * @param string $name
+ * @param mixed $data
+ * @param bool $exit
  */
-function domix( &$val, $exit = false, $filename = false ){
-	static $count = 0;
-	$count++;
-	if($filename) {
-		domix::_( $val, $exit, $count.'_'.$filename.'.txt' );
-	} else {
-		domix::_( $count );
-		domix::_( $val, $exit, false );
-	}
+function domix( $data, $exit = false ){
+	$count = domix::counter();
+    domix::_( $data, 'Row '.$count, $exit );
 }
 
+/**
+ * Gibt Daten mit domix und Firebug aus
+ *
+ * @param mixed $data
+ * @param bool $exit
+ */
+function domixf( $data, $exit = false ){
+    $count = domix::counter();
+    fb($data, 'Row '.$count);
+    domix::_( $data, 'Row '.$count, $exit );
+}
+
+/**
+ * Gibt Daten mit domix und var_dump aus
+ *
+ * @param mixed $data
+ * @param bool $exit
+ */
+function domixd( $data, $exit = false ){
+	$count = domix::counter();
+    ob_start();
+    var_dump($data);
+    $data = ob_get_clean();
+    domix::_( $data, 'Dump '.$count, $exit );
+}
